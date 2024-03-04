60361ef8f676a7eb2c407c26721a38388d709a7e
varshit.b <varshit.b@abhibus.com>
2024-03-04 12:25:29 +0530

    used drawer navigation and added screens

--- App.js
+++ App.js
@@ -1,23 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { View, StyleSheet, Button } from 'react-native';
+import {  StyleSheet } from 'react-native';
 import CalculatorScreen from './screen/CalculatorScreen';
 import LoginScreen from './screen/LoginScreen';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+const Drawer=createDrawerNavigator()
 
 export default function App() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleLogin = () => {
-    // Implement your login logic here
-    console.log('Logging in with:', { username, password });
-  };
-
   return (
-    <View style={styles.container}>
-      <LoginScreen></LoginScreen>
-      <CalculatorScreen></CalculatorScreen>
-    </View>
+    <>
+     
+        <NavigationContainer>
+          <Drawer.Navigator
+          drawerType="front"
+          initialRouteName="Login"
+          
+          >
+
+            <Drawer.Screen name="Login" component={LoginScreen}
+          options={{ 
+            // drawerIcon={<></>}
+            
+           }}
+          >
+            </Drawer.Screen>
+            <Drawer.Screen name="Calculator" component={CalculatorScreen}>
+            </Drawer.Screen>
+          </Drawer.Navigator>
+        </NavigationContainer>
+        {/* <LoginScreen></LoginScreen>
+      <CalculatorScreen></CalculatorScreen> */}
+   
+    </>
+
   );
 }
 
@@ -25,11 +44,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // backgroundColor:"black",
     justifyContent: 'center',
     alignItems: 'center',
     padding: 16,
   },
-
 });
 
